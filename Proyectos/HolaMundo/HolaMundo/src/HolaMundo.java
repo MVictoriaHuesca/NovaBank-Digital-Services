d255faba2219d3c9f13ca9d5f4673a18d121f47d
victoriahuesca <victoriahuesca@uma.es>
2026-04-14 11:24:59 +0200

    chore: init

---
+++ Proyectos/HolaMundo/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+        System.out.println();
+
+        int num = 10;
+        System.out.println("num = " + num);
+    }
+}
