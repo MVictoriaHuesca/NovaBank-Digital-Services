54995558885cb8a97f40677f578f8d7090136343
victoriahuesca <victoriahuesca@uma.es>
2026-04-14 11:44:38 +0200

    fix: unnecessary files deleted

--- Proyectos/HolaMundo/HolaMundo/src/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args){
-        System.out.println("Hola Mundo");
-        System.out.println();
-
-        int num = 10;
-        System.out.println("num = " + num);
-    }
-}
